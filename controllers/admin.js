0d3f6fad16eba27f24bbe0d84ea1b3f53eca6a99
Jonathan <jtellkampf@gmail.com>
2019-11-17 22:58:29 +0000

    Backend Skeleton without SQL database

---
+++ controllers/admin.js
@@ -0,0 +1,65 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const { title, imageUrl, description, price } = req.body;
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const { productId } = req.params;
+  Product.findById(productId, product => {
+    if (!product) {
+      return res.redirect("/");
+      // should show error instead
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const { productId, title, price, imageUrl, description } = req.body;
+  const updatedProduct = new Product(
+    productId,
+    title,
+    imageUrl,
+    description,
+    price
+  );
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products"
+    });
+  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const { productId } = req.body;
+  Product.deleteById(productId);
+  res.redirect("/admin/products");
+};
